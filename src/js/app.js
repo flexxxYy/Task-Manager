6ea7ccfc2047c143ab51e2a059859a00433d49da
flexxxYy <nickrizosss1@gmail.com>
2026-09-27 00:09:30 +0700

    Registered new format of routes

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,29 @@ import { registerRoute, navigate, renderRoute} from './router.js';
 
 import { initNavigation } from './navigation.js';
 
-import { homePage } from './pages/homePage.js';
-import { projectsPage } from './pages/projectsPage.js';
+import { homePage, initHomePage } from './pages/homePage.js';
+import { projectsPage, initProjectsPage } from './pages/projectsPage.js';
 import { statisticsPage } from './pages/statisticsPage.js';
+import { pomodoroPage } from './pages/pomodoroPage.js'
 
-registerRoute('/', homePage)
-registerRoute('/projects', projectsPage)
-registerRoute('/statistics', statisticsPage)
+registerRoute(
+    '/', 
+    homePage,
+    initHomePage
+)
+registerRoute(
+    '/projects',
+    projectsPage,
+    initProjectsPage
+);
+registerRoute(
+    '/statistics', 
+    statisticsPage
+)
+registerRoute(
+    '/pomodoro',
+    pomodoroPage
+)
 
 initNavigation();
 
